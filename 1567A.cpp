ec777292f9df35bb7e16c495353a3f5b5e19fd47
Soham Das <66131928+Soham2020@users.noreply.github.com>
2021-09-20 17:53:07 +0530

    1567A

---
+++ 1567A.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	int t;
+	cin >> t;
+	while(t--)
+	{
+		int n;
+		cin >> n;
+		string s;
+		cin >> s;
+		for(int i=0; i<s.length(); i++)
+		{
+			if(s[i] == 'U')
+				s[i] = 'D';
+			else if(s[i] == 'D')
+				s[i] = 'U';
+		}
+		for(auto x:s)
+			cout << x;
+		cout << "\n";
+	}
+}
