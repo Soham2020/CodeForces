56f465ef92368d759a5de0b603bb99c7d27a3616
Soham Das <66131928+Soham2020@users.noreply.github.com>
2021-09-28 17:47:25 +0530

    1194A

---
+++ 1194A.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long int ll;
+
+int main()
+{
+	ll t;
+	cin >> t;
+	while(t--)
+	{
+		ll n, x;
+		cin >> n >> x;
+		cout << (x*2) << "\n";
+	}
+}
