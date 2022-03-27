8f18e77851430417e853cc1b4441d03f3553a791
Soham Das <66131928+Soham2020@users.noreply.github.com>
2022-03-27 20:48:16 +0530

    1380A

---
+++ 1380A.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve()
+{
+	int n;
+	cin >> n;
+	vector <int> v(n);
+	for(int i=0; i<n; i++)
+		cin >> v[i];
+	for(int i=1; i<n-1; i++)
+	{
+		if(v[i-1] < v[i] && v[i] > v[i+1])
+		{
+			cout << "YES\n" << i << " " << i+1 << " " << i + 2 << "\n";
+			return;
+		}
+	}
+	cout << "NO\n";
+}
+
+int main()
+{
+	int t;
+	cin >> t;
+	while(t--)
+	{
+		solve();
+	}
+}
